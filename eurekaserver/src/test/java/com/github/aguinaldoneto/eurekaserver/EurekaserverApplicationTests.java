ceb52e991f74889b16269af1c78c200413ced8e5
Aguinaldo Neto <neto_aguinaldo@outlook.com>
2024-04-23 15:45:05 -0300

    Gerando o microservice do Eureka (Discovery Server)

---
+++ eurekaserver/src/test/java/com/github/aguinaldoneto/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aguinaldoneto.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
